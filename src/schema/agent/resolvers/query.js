cbffeb047b297406c52dfa76f28116e4c7445640
Luke Smetham <luke@smetham.dev>
2020-10-21 20:43:00 +0200

    added "me" query

--- src/schema/agent/resolvers/query.js
+++ src/schema/agent/resolvers/query.js
@@ -1,4 +1,20 @@
+import { AgentTC } from '../model';
+
 /**
  *! Always used named exports in resolver files.
  * export const resolverName = () => {};
  */
+
+export const me = {
+	name: 'me',
+	type: AgentTC,
+	kind: 'query',
+	args: {},
+	resolve: (_, __, { agent, models: { Agent } }) => {
+		if (!agent) {
+			throw new Error('Unauthorized');
+		}
+
+		return Agent.findById(agent, { password: false }).lean();
+	},
+};
