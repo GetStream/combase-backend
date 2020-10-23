29e2130fe3c26c27bc3c4f9d4e3e9940e5ee23f3
Luke Smetham <luke@smetham.dev>
2020-10-23 18:13:52 +0200

    tweaks to createChat mutation

--- packages/api/src/schema/chat/resolvers/mutation.js
+++ packages/api/src/schema/chat/resolvers/mutation.js
@@ -1,21 +1,17 @@
-import pubsub from 'utils/pubsub';
 import { ChatTC } from '../model';
 
 /**
  * Takes the User _id (the customer using the widget) and the organization ID as
  * arguments. Then creates a channel with just the user (ready to be routed to an agent)
  * and returns the Chat object so we can connect to the channel on the client-side.
- * /
- * We can probably use the organization ID from context down the road, but we need a way of
- * setting it from the widget.
  */
 
 export const createChat = {
 	name: 'createChat',
 	type: ChatTC,
 	kind: 'mutation',
-	args: {},
-	resolve: async (_, { user, organization }, { models: { Chat }, stream }) => {
+	args: { user: 'String!' },
+	resolve: async (_, { user }, { models: { Chat }, organization, stream }) => {
 		try {
 			const { _doc: chat } = await Chat.create({
 				organization,
@@ -26,23 +22,29 @@ export const createChat = {
 
 			await stream.chat
 				.channel('messaging', cid, {
+					created_by_id: user, // eslint-disable-line camelcase
 					members: [user],
 				})
 				.create();
 
-			// Fire internal PubSub to trigger the routing mechanism. Figure out how to send to the right queue...
-			await pubsub.publish('INTERNAL_EVENT.CHAT_CREATED', {
-				chat: cid,
-			});
+			/*
+			 * TEMP: Not working "pubsub.publish is not a function"
+			 * Fire internal PubSub to trigger the routing mechanism. Figure out how to send to the right queue...
+			 * await pubsub.publish('INTERNAL_EVENT.CHAT_CREATED', {
+			 * 	chat: cid,
+			 * });
+			 */
 
-			const PubSub = await pubsub();
+			// const PubSub = await pubsub();
 
-			PubSub.publish('event', {
-				event: 'CHAT_CREATED',
-				payload: {
-					chat: cid,
-				},
-			});
+			/*
+			 * PubSub.publish('event', {
+			 * 	event: 'CHAT_CREATED',
+			 * 	payload: {
+			 * 		chat: cid,
+			 * 	},
+			 * });
+			 */
 
 			return chat;
 		} catch (error) {
