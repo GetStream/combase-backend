05b43bdf5e3df33ef5822eee637267e7a3bd9f4e
Nick Parsons <nparsons08@gmail.com>
2020-10-29 16:39:22 -0600

    webhook

--- src/utils/webhook.js
+++ src/utils/webhook.js
@@ -14,6 +14,7 @@ const webhook = (req, res, next) => {
 		 * figure out who the webhook is coming from using identifer
 		 * check for auth type and response
 		 * route and send along req, res, etc.
+		 * get webhook id from querystring and do lookup against webhook model
 		 */
 
 		return next();
