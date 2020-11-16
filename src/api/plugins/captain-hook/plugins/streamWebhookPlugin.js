f718b1ce697d84efcc7c00aba18fce68aef8a78d
Luke Smetham <luke@smetham.dev>
2020-11-16 22:22:04 +0100

    wip

--- src/api/plugins/captain-hook/plugins/streamWebhookPlugin.js
+++ src/api/plugins/captain-hook/plugins/streamWebhookPlugin.js
@@ -1,10 +1,12 @@
 export const streamWebhookPlugin = () => ({
-	receive: (req, _, next) => {
+	receive: (req, res, next) => {
 		if (req.headers['target-agent'] === 'Stream Webhook Client') {
 			const { body: event } = req;
 
 			// eslint-disable-next-line no-console
 			console.log('received stream webhook', event.type, event);
+
+			res.send(200);
 		}
 
 		next();
