df1398474ca4714ddf8daf4e443bdd57385fdc06
Luke Smetham <luke@smetham.dev>
2020-11-18 12:48:04 +0100

    added availability to the organization model & a gql field for returning a boolean to show whether the org is currently available based on the schedule

--- src/api/schema/organization/extend.js
+++ src/api/schema/organization/extend.js
@@ -1,6 +1,40 @@
+import getDay from 'date-fns/getDay';
+import isAfter from 'date-fns/isAfter';
+import isBefore from 'date-fns/isBefore';
+import { utcToZonedTime } from 'date-fns-tz';
+
 import { OrganizationTC } from './model';
 
 OrganizationTC.addFields({
+	available: {
+		type: 'Boolean',
+		args: {},
+		resolve: async ({ _id }, _, { models: { Organization } }) => {
+			const { hours, timezone } = await Organization.findById(_id, { hours: 1 });
+
+			if (!hours?.length) {
+				// If no hours are set at all for this org, then the org is always available.
+				return true;
+			}
+
+			/** UTC Date for the user. */
+			const now = new Date();
+
+			const todayNo = getDay(now);
+
+			// If hours are set for the org, but the current day is either disabled or non-existent, return as unavailable.
+			const today = hours.find(({ day }) => day === todayNo);
+
+			if (!today?.enabled) {
+				return false;
+			}
+
+			const zonedStart = utcToZonedTime(new Date().setUTCHours(today.start.hour, today.start.minute, 0, 0), timezone);
+			const zonedEnd = utcToZonedTime(new Date().setUTCHours(today.end.hour, today.end.minute, 0, 0), timezone);
+
+			return isAfter(now, zonedStart) && isBefore(now, zonedEnd);
+		},
+	},
 	timeline: {
 		type: 'StreamFeed',
 		args: {},
