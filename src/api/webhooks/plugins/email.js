bc035214aca60f8761c2b58a226bad6751efdb5a
Nick Parsons <nparsons08@gmail.com>
2020-12-17 15:54:56 -0600

    WIP / Cleanup

--- src/api/webhooks/plugins/email.js
+++ src/api/webhooks/plugins/email.js
@@ -1,7 +1,53 @@
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+import parser from '@sendgrid/inbound-mail-parser';
+import mongoose from 'mongoose';
+import { StreamChat } from 'stream-chat';
+
+import { Models } from 'api/schema';
+import { logger } from 'utils/logger';
+
 export class CombaseEmailPlugin {
 	// eslint-disable-next-line no-unused-vars
-	handleWebhook = ({ data }) => {
-		// DO IT HERE
+	handleWebhook = async data => {
+		try {
+			const transporter = nodemailer.createTransport(
+				sgTransport({
+					auth: {
+						api_key: process.env.SENDGRID_API_KEY,
+					},
+				})
+			);
+
+			switch (data?.type) {
+				case 'email.inbound': {
+					const email = await transporter.sendMail({
+						from: '"Fred Foo 👻" <foo@example.com>', // sender address
+						to: 'bar@example.com, baz@example.com', // list of receivers
+						subject: 'Hello ✔', // Subject line
+						html: '<b>Hello world?</b>', // html body
+					});
+
+					return email;
+				}
+
+				case 'email.outbound': {
+					const email = await transporter.sendMail({
+						from: '"Fred Foo 👻" <foo@example.com>', // sender address
+						to: 'bar@example.com, baz@example.com', // list of receivers
+						subject: 'Hello ✔', // Subject line
+						html: '<b>Hello world?</b>', // html body
+					});
+
+					return email;
+				}
+
+				default:
+					return;
+			}
+		} catch (error) {
+			logger.error(error);
+		}
 	};
 
 	// data === req.body
@@ -14,12 +60,11 @@ export class CombaseEmailPlugin {
 		try {
 			for await (const event of events) {
 				// eslint-disable-next-line no-console
-				console.log('CombaseEmailPlugin:', event);
+				logger.info('CombaseEmailPlugin:', event);
 				await this.handleWebhook(event);
 			}
 		} catch (error) {
-			// eslint-disable-next-line no-console
-			console.log(error);
+			logger.error(error);
 		}
 	};
 }
