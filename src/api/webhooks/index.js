4ebb55fbb46ba336b414c31959a68cda21d29f79
Luke Smetham <luke@smetham.dev>
2021-01-13 13:12:05 +0100

    cleanup

--- src/api/webhooks/index.js
+++ src/api/webhooks/index.js
@@ -1,29 +1,28 @@
 import { CaptainHook } from '@captain-hook/core';
 import { CapnEventsEngine } from '@captain-hook/events-engine';
-import { UserModel } from 'api/schema/user/model';
 
-import { CombaseRoutingPlugin } from './plugins';
-import { CombaseEmailPlugin } from './plugins/email';
-import { CombaseActivityPlugin } from './plugins/events';
+import { UserModel } from 'api/schema/user/model';
+import { TicketModel } from 'api/schema/ticket/model';
+
+import { CombaseActivityPlugin, CombaseEmailPlugin, CombaseRoutingPlugin } from './plugins';
 import { combaseWebhookParser } from './transformers';
 import { createDynamicPlugins } from './createDynamicPlugins';
 
 import createChangeStreamSource from './createChangeStreamSource';
-import { TicketModel } from 'api/schema/ticket/model';
 
-// TODO @luke "source" plugins for captain
+const changeStreamConfig = [[], { fullDocument: 'updateLookup' }];
+/*
+ * const ChangeStreamSource = createChangeStreamSource([UserModel.watch(...changeStreamConfig), TicketModel.watch(...changeStreamConfig)]);
+ * TODO @luke "source" plugins for captain
+ */
 /** Source plugin is a plugin that only publishes and listens to another source (i.e. change streams, not capn itself) this allows other events to be piped through capn, similar to onWebhook but for SDK or `stream` based payloads */
 const capn = new CaptainHook({
 	engine: CapnEventsEngine, // TODO replace with Rabbit Engine
 	plugins: [CombaseRoutingPlugin, CombaseActivityPlugin, CombaseEmailPlugin, ...createDynamicPlugins()],
 	pre: [combaseWebhookParser],
+	// source: [ChangeStreamSource],
 });
 
-const changeStreamConfig = [[], { fullDocument: 'updateLookup' }];
-
-export const asyncChangeStream = createChangeStreamSource(capn, [
-	UserModel.watch(...changeStreamConfig),
-	TicketModel.watch(...changeStreamConfig),
-]);
+createChangeStreamSource(capn, [UserModel.watch(...changeStreamConfig), TicketModel.watch(...changeStreamConfig)]);
 
 export default capn;
