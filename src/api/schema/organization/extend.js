ed79fffd25decd453d08e5d1aca62a9c27565ad9
Luke Smetham <luke@smetham.dev>
2021-01-14 14:54:41 +0100

    cleanup

--- src/api/schema/organization/extend.js
+++ src/api/schema/organization/extend.js
@@ -1,29 +1,3 @@
 import { OrganizationTC } from './model';
 
-OrganizationTC.addNestedFields({
-	'stream.key': {
-		type: 'String!',
-		args: {},
-		resolve: async (source, _, { models: { Organization }, organization }) => {
-			if (!organization) {
-				throw new Error('Unauthorized');
-			}
-
-			try {
-				const { stream } = await Organization.findById(organization, { 'stream.key': true });
-
-				if (source.key === stream?.key) {
-					const decrypted = await Organization.findOne({ _id: organization }, { stream: true });
-
-					return decrypted?.stream?.key;
-				}
-
-				return null;
-			} catch (error) {
-				throw new Error(error.message);
-			}
-		},
-	},
-});
-
 OrganizationTC.removeField('stream');
