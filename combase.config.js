9f204de02dfa61a8f0d6211fe65caa20c2311baf
Luke Smetham <luke@arkitek.co.uk>
2021-01-27 13:57:28 +0100

    fixed up prepare scripts now that the plugins are published

--- combase.config.js
+++ combase.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-	plugins: ['@combase.app/plugin-sendgrid', '@combase.app/plugin-stream-activity-feeds', '@combase.app/plugin-autorouting'],
+	plugins: [
+		'@combase.app/plugin-sendgrid',
+		'@combase.app/plugin-zendesk',
+		'@combase.app/plugin-stream-activity-feeds',
+		'@combase.app/plugin-autorouting',
+	],
 };
--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const ora = require('ora');
 const slash = require('slash');
 const path = require('path');
 const fs = require('fs-extra');
@@ -14,17 +13,11 @@ const dataDir = path.join(process.cwd(), '.data');
 (async () => {
 	await fs.ensureDir(slash(dataDir));
 
-	let spinner;
-
 	if (process.env.SKIP_PLUGIN_INSTALL !== 'true') {
-		spinner = ora('Installing Combase plugins').start();
-
 		await installPlugins();
 	}
 
 	const plugins = await loadPlugins(config);
-
-	spinner.succeed();
 
 	// eslint-disable-next-line no-sync
 	await fs.writeFile(slash(path.join(dataDir, 'integration-manifest.json')), JSON.stringify(plugins));
