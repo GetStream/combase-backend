0a7f2ed9fc632541ff74aa9e43cd2d608a4854c0
Luke Smetham <luke@arkitek.co.uk>
2021-03-18 15:42:34 +0100

    tweaked the FAQ model

--- src/api/schema/faq/model.js
+++ src/api/schema/faq/model.js
@@ -4,6 +4,17 @@ import { composeMongoose } from 'graphql-compose-mongoose';
 
 import schemaComposer from 'api/schema/composer';
 
+const defaultContent = [
+	{
+		children: [
+			{
+				children: [{ text: '' }],
+				type: 'h1',
+			},
+		],
+	},
+];
+
 const FaqSchema = new Schema(
 	{
 		organization: {
@@ -16,21 +27,31 @@ const FaqSchema = new Schema(
 			{
 				type: Schema.Types.ObjectId,
 				ref: 'Tag',
-				required: true,
 				description: 'Tag used to categorize a FAQ.',
 			},
 		],
 		title: {
 			type: String,
 			trim: true,
-			required: true,
 			description: 'Title of the FAQ.',
 		},
 		body: {
 			type: String,
 			trim: true,
-			required: true,
 			description: 'Body of the FAQ.',
+		},
+		content: {
+			type: JSON,
+			required: true,
+			default: defaultContent,
+			description:
+				'Original Slate JSON from the editor: used as a backup to re-serialize content if necessary, or on the fly. Title & Body should be serialized when the document is updated',
+		},
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			ref: 'Agent',
+			required: true,
+			description: 'A reference to the agent that created this FAQ article',
 		},
 	},
 	{ collection: 'faqs' }
