88bd257e1910c236e39d8529bde48d19fa520860
Luke Smetham <luke@smetham.dev>
2021-03-21 01:09:44 +0100

    Update query.js

--- src/api/schema/faq/modifiers/query.js
+++ src/api/schema/faq/modifiers/query.js
@@ -1,2 +1,25 @@
-export const faq = tc => tc.mongooseResolvers.findById().clone({ name: 'get' });
+export const faq = tc =>
+	tc.mongooseResolvers
+		.findOne()
+		.wrap(resolver => {
+			// eslint-disable-next-line no-param-reassign
+			resolver.args = {
+				shortId: 'String!',
+			};
+
+			return resolver;
+		})
+		.wrapResolve(next => rp => {
+			const { shortId } = rp.args;
+
+			// eslint-disable-next-line no-param-reassign
+			rp.args = {
+				filter: {
+					shortId,
+				},
+			};
+
+			return next(rp);
+		})
+		.clone({ name: 'get' });
 export const faqs = tc => tc.mongooseResolvers.connection().clone({ name: 'list' });
