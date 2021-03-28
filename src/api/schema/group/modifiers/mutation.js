9869866d91e2690b453357091ba779c3bc580c0d
Luke Smetham <luke@smetham.dev>
2021-03-28 17:27:40 +0200

    switch *One to *ById

--- src/api/schema/group/modifiers/mutation.js
+++ src/api/schema/group/modifiers/mutation.js
@@ -1,8 +1,8 @@
 import { createAddTagResolver, createRemoveTagResolver } from 'utils/createTaggableEntity';
 
 export const groupCreate = tc => tc.mongooseResolvers.createOne().clone({ name: 'create' });
-export const groupUpdate = tc => tc.mongooseResolvers.updateOne().clone({ name: 'update' });
-export const groupRemove = tc => tc.mongooseResolvers.removeOne().clone({ name: 'remove' });
+export const groupUpdate = tc => tc.mongooseResolvers.updateById().clone({ name: 'update' });
+export const groupRemove = tc => tc.mongooseResolvers.removeById().clone({ name: 'remove' });
 export const groupRemoveMany = tc => tc.mongooseResolvers.removeMany().clone({ name: 'removeMany' });
 
 export const groupAddTag = tc => createAddTagResolver(tc);
