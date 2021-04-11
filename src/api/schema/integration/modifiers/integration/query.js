3ba1cab6f663ce11565d60cec4a7714056858757
Luke Smetham <luke@arkitek.co.uk>
2021-04-12 00:20:18 +0200

    integrationLookup r esolver

--- src/api/schema/integration/modifiers/integration/query.js
+++ src/api/schema/integration/modifiers/integration/query.js
@@ -1,2 +1,17 @@
+import { IntegrationModel } from "../../model";
+
+export const integrationLookup = tc => tc.schemaComposer.createResolver({
+	name: 'lookup',
+	type: tc,
+	args: {
+		organization: 'MongoID!',
+		triggers: 'JSON!',
+	},
+	resolve: ({ args }) => {
+		return IntegrationModel.findOne(args);
+	}
+})
+
 export const integration = tc => tc.mongooseResolvers.findById().clone({ name: 'get' });
+
 export const integrations = tc => tc.mongooseResolvers.connection().clone({ name: 'list' });
