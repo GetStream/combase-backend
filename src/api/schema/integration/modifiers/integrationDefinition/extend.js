108f93b5c474d9d226b7ac27b672202f366041c8
Luke Smetham <luke@smetham.dev>
2021-05-25 11:33:08 +0100

    added relation between integrationDefinitions and currently active integrations for the authed org

---
+++ src/api/schema/integration/modifiers/integrationDefinition/extend.js
@@ -0,0 +1,28 @@
+export const extend = tc => {
+	const IntegrationTC = tc.schemaComposer.getOTC('Integration');
+
+	tc.addFields({
+		integrationData: {
+			type: IntegrationTC,
+			projection: {
+				id: true,
+			},
+			resolve: (source, _, context, info) => {
+				const { organization } = context;
+
+				if (!organization) {
+					throw new Error('Unauthorized.');
+				}
+
+				const { id: uid } = source;
+
+				const args = {
+					uid,
+					organization,
+				};
+
+				return IntegrationTC.getResolver('lookup').resolve({ source, args, context, info });
+			},
+		},
+	});
+};
